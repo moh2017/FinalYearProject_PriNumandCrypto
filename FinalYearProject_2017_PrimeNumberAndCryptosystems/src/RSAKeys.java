46bd42c82537e2f18b5e9a0e8a916a1ca856b76f
Mahmoud <Mahmoud@MyPc>
2017-11-21 21:58:10 +0000

    Implementing the get_e method in the RSAKeys class

--- FinalYearProject_2017_PrimeNumberAndCryptosystems/src/RSAKeys.java
+++ FinalYearProject_2017_PrimeNumberAndCryptosystems/src/RSAKeys.java
@@ -23,16 +23,56 @@ public class RSAKeys {
    * 
    * @retur
    */
-  public BigInteger getp(){
+  public BigInteger get_p(){
     p = new BigInteger(bit_length/2,10,rand);
     return p;
   }
   
-  public BigInteger getq(){
+  /**
+   * 
+   * @return
+   */
+  public BigInteger get_q(){
     do{
       q = new BigInteger(bit_length/2,10,rand);
     }while(q.compareTo(p) == 0);
     return q;
   }
+  
+  /**
+   * n = p * q.
+   * @return
+   */
+  public BigInteger get_n(){
+    n = p.multiply(q);
+    return n;
+  }
+  
+  /**
+   * Phi_n = p-1 * q-1.
+   * 
+   * @return
+   */
+  public BigInteger getPhi_n(){
+    phi_n = p.subtract(BigInteger.ONE)
+        .multiply(q.subtract(BigInteger.ONE));
+    return phi_n;
+  }
+  
+  /**
+   * Randomly choose e as long as e > 0 
+   * Or e < phi_n 
+   * @return
+   */
+  public BigInteger get_e(){
+    do{
+      e = new BigInteger(phi_n.bitLength(),rand);
+    }while(e.compareTo(BigInteger.ONE) <= 0
+        || e.compareTo(phi_n) >= 0
+        || !e.gcd(phi_n).equals(BigInteger.ONE));
+    
+    return e;
+  }
+  
 }
 
